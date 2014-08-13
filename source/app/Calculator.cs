fe9a3eaa3dca51d2cea1f8704047d5360dc0de19
cbenien <christian.benien@philips.com>
2014-08-13 10:47:59 +0200

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,9 +1,14 @@
-﻿namespace app
+﻿using System;
+
+namespace app
 {
   public class Calculator
   {
     public int add(int i, int i1)
     {
+        if (i < 0) throw new ArgumentException("I can only do positive numbers", "i");
+        if (i1 < 0) throw new ArgumentException("I can only do positive numbers", "i1");
+
         return i + i1;
     }
   }
