0b7608dcf82cca52af57de1addc2292fced5f908
cbenien <christian.benien@philips.com>
2014-08-13 14:36:33 +0200

    Pushing new changes

--- source/app/web/application/catalog_browsing/ViewTheMainDepartments.cs
+++ source/app/web/application/catalog_browsing/ViewTheMainDepartments.cs
@@ -2,11 +2,18 @@
 
 namespace app.web.application.catalog_browsing
 {
-  public class ViewTheMainDepartments : ISupportAUserStory
-  {
-    public void process(IProvideRequestDetails request)
-    {
-      throw new System.NotImplementedException();
-    }
-  }
+	public class ViewTheMainDepartments : ISupportAUserStory
+	{
+		private IFindDepartments department_finder;
+
+		public ViewTheMainDepartments(IFindDepartments departmentFinder)
+		{
+			department_finder = departmentFinder;
+		}
+
+		public void process(IProvideRequestDetails request)
+		{
+			department_finder.get_the_main_departments();
+		}
+	}
 }
