dadfde1f70e646acd76d8e770ad22c94cb25a86e
cbenien <christian.benien@philips.com>
2014-08-14 13:46:58 +0200

    Pushing new changes

---
+++ source/app.specs/ViewTheProductsInADepartmentSpecs.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using app.web.application.catalog_browsing;
+using app.web.core;
+using developwithpassion.specifications.extensions;
+using developwithpassion.specifications.rhinomocks;
+using Machine.Specifications;
+using Rhino.Mocks.Constraints;
+
+namespace app.specs
+{
+	[Subject(typeof(ViewTheProductsInADepartment))]
+	public class ViewTheProductsInADepartmentSpecs
+	{
+		public abstract class concern : Observes<ISupportAUserStory,
+		  ViewTheProductsInADepartment>
+		{
+		}
+
+		public class when_run : concern
+		{
+			Establish c = () =>
+			{
+				display_engine = depends.on<IDisplayInformation>();
+				product_finder = depends.on<IFindProducts>();
+				request = fake.an<IProvideRequestDetails>();
+				input = new ProductsInDepartmentInput();
+				request.setup(x => x.map<ProductsInDepartmentInput>()).Return(input);
+
+				products = Enumerable.Range(1, 100).Select(x => new ProductLineItem());
+				product_finder.setup(x => x.get_products_using(input)).Return(products);
+			};
+
+			Because b = () =>
+			  sut.process(request);
+
+			It displays_the_departments = () =>
+			  display_engine.received(x => x.display(products));
+
+			private static ProductsInDepartmentInput input;
+			static IFindProducts product_finder;
+			static IProvideRequestDetails request;
+			static IDisplayInformation display_engine;
+			private static IEnumerable<ProductLineItem> products;
+		}
+	}
+}
---
+++ source/app/web/application/catalog_browsing/ViewTheProductsInADepartment.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using app.web.application.catalog_browsing.stubs;
+using app.web.aspnet;
+using app.web.core;
+
+namespace app.web.application.catalog_browsing
+{
+	public class ViewTheProductsInADepartment : ISupportAUserStory
+	{
+		IFindProducts product_finder;
+		IDisplayInformation display;
+
+		public ViewTheProductsInADepartment(IFindProducts product_finder, IDisplayInformation display)
+		{
+			this.product_finder = product_finder;
+			this.display = display;
+		}
+
+		public void process(IProvideRequestDetails request)
+		{
+			var input = request.map<ProductsInDepartmentInput>();
+			var results = product_finder.get_products_using(input);
+
+			display.display(results);
+		}
+	}
+
+	public class ProductsInDepartmentInput
+	{
+	}
+
+	public interface IFindProducts
+	{
+		IEnumerable<ProductLineItem> get_products_using(ProductsInDepartmentInput input);
+	}
+
+	public class ProductLineItem
+	{
+		
+	}
+
+}
